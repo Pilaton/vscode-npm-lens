2f709d1690a95c479732a4d9093e16bd282f91b2
Pilaton <dev@pilaton.com>
2024-12-03 14:42:29 +0300

    refactor: Adaptive layout in "short view" has been corrected

--- src/webview/components/Accordion/info-short.tsx
+++ src/webview/components/Accordion/info-short.tsx
@@ -2,7 +2,7 @@ import CheckCircleIcon from '@mui/icons-material/CheckCircle';
 import ContentCopyIcon from '@mui/icons-material/ContentCopy';
 import Box from '@mui/material/Box';
 import Button from '@mui/material/Button';
-import Grid from '@mui/material/Grid';
+import Stack from '@mui/material/Stack';
 import copy from 'copy-to-clipboard';
 import { useEffect, useState } from 'react';
 
@@ -32,44 +32,37 @@ export default function InfoShort({
   }, [isCopied]);
 
   return (
-    <>
-      <Grid
-        container
+    <Stack direction="row" spacing={2} sx={{ width: '100%', alignItems: 'center' }}>
+      <Stack
+        direction="row"
         spacing={1}
-        sx={{ width: '40%', flexShrink: 0, alignItems: 'center', fontSize: '.9375rem' }}
+        sx={{ width: '40%', alignItems: 'center', flexShrink: 0, fontSize: '.9375rem' }}
       >
-        <Grid item>{name}</Grid>
+        <Box>{name}</Box>
 
         {expanded && (
-          <Grid
-            item
-            sx={{}}
+          <Button
+            size="small"
+            sx={{ minWidth: 0 }}
             onClick={(event) => {
               event.stopPropagation();
+              copy(name) && setIsCopied(true);
             }}
           >
-            <Button
-              size="small"
-              sx={{ minWidth: 0 }}
-              onClick={() => {
-                copy(name) && setIsCopied(true);
-              }}
-            >
-              {isCopied ? (
-                <CheckCircleIcon color="success" sx={{ fontSize: '1rem' }} />
-              ) : (
-                <ContentCopyIcon sx={{ fontSize: '1rem' }} />
-              )}
-            </Button>
-          </Grid>
+            {isCopied ? (
+              <CheckCircleIcon color="success" sx={{ fontSize: '1rem' }} />
+            ) : (
+              <ContentCopyIcon sx={{ fontSize: '1rem' }} />
+            )}
+          </Button>
         )}
-      </Grid>
+      </Stack>
 
       <Box sx={{ width: '20%', textAlign: 'right', letterSpacing: '0.75px' }}>{currentVersion}</Box>
 
       {children}
 
       <Box sx={{ width: '10%' }} />
-    </>
+    </Stack>
   );
 }
